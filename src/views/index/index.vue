5da73fa66ff7033221e56a3dd5039282cf23acc5
chendj89@hotmail.com <719857499@qq.com>
2023-05-10 11:37:06 +0800

    加密解密算法+密钥

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -6,27 +6,41 @@
   </div>
 </template>
 <script lang="ts" setup name="Index">
-import loadVirtual from "@/virtual";
-import uDropdown from "@/unity/uDropdown";
+import loadVirtual from '@/virtual'
+import uDropdown from '@/unity/uDropdown'
+import { encrypt, decrypt } from '@/utils/encryption'
+import { useRouter } from 'vue-router'
 
-const router = useRouter();
+const router = useRouter()
+
 /**
  * 添加动态路由
  */
 const addRoute = () => {
-  const name = "virtual1";
+  const name = 'virtual1'
   if (!router.hasRoute(name)) {
-    loadVirtual({ name });
+    loadVirtual({ name })
   }
-  router.push(name);
-};
+  router.push(name)
+}
 
 const options = [
   {
-    label: "用户名",
-    key: "username",
-  },
-];
+    label: '用户名',
+    key: 'username'
+  }
+]
+
+const myKey = '九转灵山'
+const myData = {
+  name: myKey,
+  version: '1.0.0',
+  menu: ['1', 2, 3]
+}
+let str = encrypt(myData, myKey)
+console.log('src', str)
+let unstr = decrypt(str, myKey)
+console.log('unstr', unstr)
 </script>
 
 <style scoped></style>
